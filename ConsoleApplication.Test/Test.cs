3582d01ecf83af997dec38aea507fd723f67b5af
christian.wigren <christian.wigren@valtech.se>
2013-11-12 01:49:46 +0100

    added console app instead

---
+++ ConsoleApplication.Test/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace ConsoleApplication.Test
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
